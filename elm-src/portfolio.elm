1e7e73a971c4619f734347303e821ca63ec832c2
kalz2q <katsu95i@gmail.com>
2019-04-29 19:27:45 +0900

    m

--- elm-src/portfolio.elm
+++ elm-src/portfolio.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module Portfolio exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (..)
@@ -7,38 +7,55 @@ import Html.Events exposing (onClick)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
 init =
-  0
+    0
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ img [src "humanblack.png", width 140 , alt "太郎" , class "icon"][]
-    , h1 [ ] [ text "山田太郎" ]
-    , p [] [ text  "ui/uxデザイナー見習いです" ]
-    ]
+    div []
+        -- HOME
+        -- ABOUT
+        [ img [ src "header.png", width 400, height 130, alt "ヘッダー画像" ] []
+        , img [ src "humanblack.png", width 140, alt "太郎", class "icon" ] []
+        , h1 [] [ text "山田太郎" ]
+        , p [] [ text "ui/uxデザイナー見習いです" ]
+        , img [ src "shumei.png", width 140, alt "太郎", alt "州名ゲーム" ] []
+        , h2 [] [ text "州名ゲーム" ]
+        , p [] [text "楽しいアプリです"]
+        ]
